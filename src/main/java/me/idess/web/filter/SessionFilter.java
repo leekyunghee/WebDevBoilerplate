381f38afad43d48cd8d7c15ccc75ccdd6b419c7b
idess <m.idess@gmail.com>
2014-07-31 17:21:03 +0900

    formatter

--- src/main/java/me/idess/web/filter/SessionFilter.java
+++ src/main/java/me/idess/web/filter/SessionFilter.java
@@ -19,75 +19,74 @@ import javax.servlet.http.HttpSession;
 
 import me.idess.web.common.Constants;
 import me.idess.web.model.TokenObject;
-import me.idess.web.util.ObjectUtil;
 import net.sf.json.JSONObject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class SessionFilter implements Filter {
-
-	private static final Logger logger = LoggerFactory.getLogger(SessionFilter.class);
+	
+	private static final Logger	logger	= LoggerFactory.getLogger(SessionFilter.class);
 	
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
 		logger.debug("destroy Call");
 	}
-
+	
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
 			FilterChain filterChain) throws IOException, ServletException {
 		// TODO Auto-generated method stub
 		logger.debug("doFilter Call");
 		
-		String serviceCode = "";
+		// String serviceCode = "";
 		boolean tokenStatus = false;
 		
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
 		
 		String uri = request.getRequestURI();
-		logger.debug("uri["+uri+"]");
+		logger.debug("uri[" + uri + "]");
 		
 		HttpSession httpSession = request.getSession();
 		
 		try {
+			RequestWrapper requestWrapper = new RequestWrapper(request);
+			String requestBody = requestWrapper.getBody();
 			
 			// resources 파일 필터 조건 제외
-			if(uri.contains(".js") || uri.contains(".css") || uri.contains(".gif") 
-					|| uri.contains(".png") || uri.contains(".jpg") || uri.contains(".ico") 
-					|| uri.contains(Constants.Template_File_Path)
-					) {
+			if (uri.contains(".js") || uri.contains(".css") || uri.contains(".gif")
+					|| uri.contains(".png") || uri.contains(".jpg") || uri.contains(".ico")
+					|| uri.contains(Constants.Template_File_Path)) {
 				filterChain.doFilter(servletRequest, servletResponse);
 				return;
 			}
 			
 			// 초기 페이지 및 로그인 페이지로 갈경우 세션 제거
-			if(uri.equals("/") || uri.equals("/index.html")) {
+			if (uri.equals("/") || uri.equals("/index.html")) {
 				httpSession.removeAttribute("Token");
 				httpSession.removeAttribute("Account");
 				logger.debug("init/index page ; session removed");
 			}
 			
-			if (request.getParameter("q") != null) {
-				logger.debug("q data is not null");
-				String q = request.getParameter("q").toString();
-				JSONObject jsonObject = JSONObject.fromObject(q);
+			if (requestBody != null) {
+				logger.debug("requestBody is not null");
+				JSONObject jsonObject = JSONObject.fromObject(requestBody);
 				JSONObject dataInfoObject = (JSONObject) jsonObject.get(Constants.DataInfo);
 				JSONObject headerObject = (JSONObject) dataInfoObject.get(Constants.Header);
 				JSONObject bodyObject = (JSONObject) dataInfoObject.get(Constants.Body);
 				
-				serviceCode = ObjectUtil.isNull(headerObject.get("ServiceCode"), "");
-			
+				// serviceCode = ObjectUtil.isNull(headerObject.get("ServiceCode"), "");
+				
 				// 세션이 존재하는지 확인
 				if (httpSession.getAttribute("Token") != null) {
 					logger.debug("세션 존재");
 					
 					// 현재 토큰이 유효하다면
 					if (TokenObject.getToken(httpSession.getAttribute("Account").toString()) != null
-							&& TokenObject.getToken(httpSession.getAttribute("Account").toString()).equals(httpSession.getAttribute("Token").toString())) {
+							&& TokenObject.getToken(httpSession.getAttribute("Account").toString())
+									.equals(httpSession.getAttribute("Token").toString())) {
 						logger.debug("토큰 유효");
 						headerObject.put("Token", httpSession.getAttribute("Token").toString());
 						headerObject.put("Account", httpSession.getAttribute("Account").toString());
@@ -100,13 +99,16 @@ public class SessionFilter implements Filter {
 					}
 					
 				}
-				//세션이 존재하지 않으면
+				// 세션이 존재하지 않으면
 				else {
-					//로그인 서비스일 경우
+					// 로그인 서비스일 경우
 					logger.debug("세션 존재하지 않음");
-					if (serviceCode.equals("loginServiceCode") || uri.equals("/login")) {
-						//headerObject.put("Account", ((JSONObject)((JSONObject) bodyObject.get(Constants.Parameter)).get(Constants.LoginKey)).get(Constants.LoginId));
-						headerObject.put("Account", ((JSONObject) bodyObject.get(Constants.Parameter)).get(Constants.LoginId));
+					// if (serviceCode.equals("loginServiceCode") || uri.equals("/login")) {
+					if (uri.equals("/login")) {
+						// headerObject.put("Account", ((JSONObject)((JSONObject)
+						// bodyObject.get(Constants.Parameter)).get(Constants.LoginKey)).get(Constants.LoginId));
+						headerObject.put("Account", ((JSONObject) bodyObject
+								.get(Constants.Parameter)).get(Constants.LoginId));
 					}
 				}
 				
@@ -118,13 +120,14 @@ public class SessionFilter implements Filter {
 			
 			if (httpSession.getAttribute("Token") != null) {
 				
-				if (serviceCode.equals("logoutServiceCode") || uri.equals("/logout")){
+				// if (serviceCode.equals("logoutServiceCode") || uri.equals("/logout")){
+				if (uri.equals("/logout")) {
 					// 토큰이 유효하지 않을 경우 세션 삭제
 					httpSession.removeAttribute("Token");
 					httpSession.removeAttribute("Account");
 					response.sendRedirect("/");
-					//RequestDispatcher rd = request.getRequestDispatcher("/");
-					//rd.forward(request, response);
+					// RequestDispatcher rd = request.getRequestDispatcher("/");
+					// rd.forward(request, response);
 					logger.debug("logout service call; delete session");
 					return;
 				} else {
@@ -133,31 +136,35 @@ public class SessionFilter implements Filter {
 					logger.debug("Session is not null and normal url access");
 					return;
 				}
-
 			} else {
-				if (serviceCode.equals("loginServiceCode") || uri.equals("/login") || uri.equals("/") || uri.equals("/index.html")) {
+				// if (serviceCode.equals("loginServiceCode") || uri.equals("/login") ||
+				// uri.equals("/") || uri.equals("/index.html")) {
+				if (uri.equals("/login") || uri.equals("/") || uri.equals("/index.html")) {
 					filterChain.doFilter(request, response);
-				} else {	// 비정상적인 요청일 경우
+				} else { // 비정상적인 요청일 경우
 					response.sendRedirect("/");
 					logger.debug("Session is null or abnormal url access");
-					//filterChain.doFilter(servletRequest, servletResponse);
+					// filterChain.doFilter(servletRequest, servletResponse);
 					return;
 				}
 			}
 			
-			if ((serviceCode.equals("loginServiceCode") || uri.equals("/login"))&& request.getAttribute("q") != null) {
-				String q = request.getAttribute("q").toString();
-				JSONObject jsonObject = JSONObject.fromObject(q);
-				JSONObject dataInfoObject = (JSONObject) jsonObject.get(Constants.DataInfo);
-				JSONObject bodyObject = (JSONObject) dataInfoObject.get(Constants.Body);
-				JSONObject resultDataObject = (JSONObject) bodyObject.get(Constants.ResultData);
-				JSONObject loginInfo = (JSONObject) resultDataObject.get("Login");
+			// if ((serviceCode.equals("loginServiceCode") || uri.equals("/login"))&&
+			// request.getAttribute("q") != null) {
+			if (uri.equals("/login") && requestBody != null) {
+				JSONObject jsonObject = JSONObject.fromObject(requestBody);
 				
 				// 로그인 성공일 경우 세션 생성
-				if (loginInfo != null && "Y".equals(loginInfo.get("successYn"))) {
-					httpSession.setAttribute("Account", loginInfo.get(Constants.LoginId));
-					httpSession.setAttribute("Token", loginInfo.get("token"));
-					logger.debug("login success ; add session > Token["+httpSession.getAttribute("Token")+"]");
+				if (jsonObject != null && "Y".equals(jsonObject.get("successLogin"))) {
+					httpSession.setAttribute("Account", jsonObject.get("username"));
+					httpSession.setAttribute("Token", jsonObject.get("token"));
+					
+					// token을 메모리에 설정
+					TokenObject.setToken((String) jsonObject.get(Constants.LoginId),
+							(String) jsonObject.get("token"));
+					
+					logger.debug("login success ; add session > Token["
+							+ httpSession.getAttribute("Token") + "]");
 				}
 			}
 			
@@ -166,11 +173,11 @@ public class SessionFilter implements Filter {
 		}
 		
 	}
-
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		// TODO Auto-generated method stub
 		logger.debug("init Call");
 	}
-
+	
 }
