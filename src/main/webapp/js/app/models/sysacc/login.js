6548b22b0f287caafb42c731e0b007401aeecc83
idess <m.idess@gmail.com>
2014-08-04 14:26:10 +0900

    change rest api path

--- src/main/webapp/js/app/models/sysacc/login.js
+++ src/main/webapp/js/app/models/sysacc/login.js
@@ -10,7 +10,7 @@ define(function(require) {
 			"password" : "",
 			"successLogin" : ""
 		},
-		url : "login",
+		url : "api/login",
 		obtainCertification : function(options) {
 			this.fetch({
 				method : "POST",
