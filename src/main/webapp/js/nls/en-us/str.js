4b1433cad409d9d4c6c9eff2ad6defa4951c042c
idess <m.idess@gmail.com>
2014-08-07 14:32:29 +0900

    용어 추가

--- src/main/webapp/js/nls/en-us/str.js
+++ src/main/webapp/js/nls/en-us/str.js
@@ -1,5 +1,8 @@
 define({
 	"id" : "User ID",
 	"password" : "Password",
-	"sign_in" : "Sign In"
+	"sign_in" : "Sign In",
+	"close" : "Close",
+	"cancel" : "Cancel",
+	"ok" : "OK"
 });
--- src/main/webapp/js/nls/str.js
+++ src/main/webapp/js/nls/str.js
@@ -3,7 +3,10 @@ define({
 	"root" : {
 		"id" : "아이디",
 		"password" : "비밀번호",
-		"sign_in" : "로그인"
+		"sign_in" : "로그인",
+		"close" : "닫기",
+		"cancel" : "취소",
+		"ok" : "확인"
 	},
 	"en-us" : true
 });
