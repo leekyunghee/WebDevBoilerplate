80b1112dd64cbb1cd3e6cd79d26d3bd92a833907
idess <m.idess@gmail.com>
2014-08-22 16:55:37 +0900

    사용자 한명의 정보를 보여주는 View 개발

---
+++ src/main/webapp/js/app/views/system/account/detail.js
@@ -0,0 +1,54 @@
+define(function(require) {
+
+	"use strict";
+
+	// require library
+	var $ = require('jquery'),
+		_ = require('underscore'),
+		Backbone = require('backbone'),
+		tpl = require('text!tpl/system/account/accountDetail.html');
+
+	// require model
+	var AccountModel = require('models/system/account'),
+		accountModel = new AccountModel();
+
+	var template = _.template(tpl);
+
+	return Backbone.View.extend({
+
+		initialize : function() {
+			console.log('account/list.js');
+		},
+
+		render : function(options) {
+			var self = this;
+			this.$el.empty();
+
+			accountModel.set({
+				username : options.username
+			}, {
+				silent : true,
+				validate : false
+			});
+			accountModel.fetch({
+				method : "POST",
+				contentType : 'application/json',
+				data : JSON.stringify(accountModel.toJSON()),
+				success : function(model, response, options) {
+					console.log(model);
+					self.$el.html(template(model.attributes));
+				},
+				error : function(model, response) {
+					console.log('fetch error');
+					console.log(model);
+					console.log(response);
+				}
+			});
+			
+			options.listView.selectAccountItem(options.username);
+
+			return this;
+		}
+	});
+
+});
